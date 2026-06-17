60497ea15b3c89e12d5f79e474ad4e8d99cc497b
ravy <macbookpro@MacBooks-MacBook-Pro.local>
2026-06-17 15:45:56 +0700

    last code update

--- obj/Release/net8.0/VQuery.AssemblyInfo.cs
+++ obj/Release/net8.0/VQuery.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ravy")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38b67a42e2034c646df904e257cdc0d536c6db7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("VQuery")]
